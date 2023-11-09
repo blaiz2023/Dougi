9993196251c47793563e3016daca7070b8439fae
Blaiz Enterprises <contact@blaizenterprises.com>
2023-11-10 00:21:08 +1030

    Add files via upload

---
+++ Dougi.dpr
@@ -0,0 +1,13 @@
+program Dougi;
+
+uses
+  Dougi1 in 'Dougi1.pas' {Form1},
+  gosscore in 'gosscore.pas',
+  Dougi2 in 'Dougi2.pas',
+  gossdat in 'gossdat.pas';
+
+{$R *.RES}
+
+begin
+app__init;
+end.
